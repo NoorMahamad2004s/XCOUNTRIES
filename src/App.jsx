f6ba4e4d8114e5157c6fa40c4841ee0f5b090e45
NoorMahamad <noormahamad.s21@gmail.com>
2024-05-09 22:10:01 +0530

    Countries Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Cards from './Components/Countries/Countries'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Cards />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,43 @@
+
+import './Countries.css';
+import { useState, useEffect } from 'react';
+import propTypes from 'react';
+
+
+const Tile = (props)=>{
+
+    return(
+        <div className="card">
+            <img className='cardImg' src={props.src} alt={props.alt} />
+            <h4>{props.name}</h4>
+        </div>
+    )
+}
+
+
+const Cards = ()=>{
+    const [countires, setCountries] = useState([]);
+    const API_URL = "https://restcountries.com/v3.1/all";
+
+    useEffect(()=>{
+        fetch(API_URL)
+        .then((response)=>response.json())
+        .then((data)=>setCountries(data))
+    },[])
+
+console.log(countires);
+
+    return(
+        <div className="cards-container">
+            {countires.map((country)=><Tile key={country.cca3} src={country.flags.png} alt={country.flags.alt} name={country.name.common} />)}
+        </div>
+    )
+}
+
+Tile.propTypes = {
+    src:propTypes.string,
+    alt:propTypes.string,
+    name:propTypes.string
+}
+
+export default Cards;
